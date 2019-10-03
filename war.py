b17ca05df123f00fbd9b675bd22006ccd15a71da
apierc2175 <aidanwpierce@gmail.com>
2019-10-03 11:21:54 -0400

    added values for cards

--- war.py
+++ war.py
@@ -1,14 +1,34 @@
-user_name = input("Please type your name here: ")
-print("User Name: " + user_name)
+#amount of cards each have, if one person gets 52 cards then they win, if you win round your count increases by two
+computer_card_number = 26
+user_card_number = 26
+
+class Player:
+    user_name = input("Please type your name here: ")
+    print("User Name: " + user_name)
 
 class Card:
-    print("test card")
+    #setting value for each card, so can say if the cards value is higher than the other card you win
+    card_value = {
+      "1": 1,
+      "2": 2,
+      "3": 3,
+      "4": 4,
+      "5": 5,
+      "6": 6,
+      "7": 7,
+      "8": 8,
+      "9": 9,
+      "10": 10,
+      "J": 11,
+      "Q": 12,
+      "K": 13,
+      "A": 14
+    }
+
+    print(card_value)
 
 class Deck:
     print("test deck")
-
-class Player:
-    print("test player")
 
 class Game:
     print("test game")
