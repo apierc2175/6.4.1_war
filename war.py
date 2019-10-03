9ddaf56ba73447594272a631a891bea504117380
apierc2175 <aidanwpierce@gmail.com>
2019-10-03 11:02:59 -0400

    set up username and basic class outline

---
+++ war.py
@@ -0,0 +1,14 @@
+user_name = input("Please type your name here: ")
+print("User Name: " + user_name)
+
+class Card:
+    print("test card")
+
+class Deck:
+    print("test deck")
+
+class Player:
+    print("test player")
+
+class Game:
+    print("test game")
